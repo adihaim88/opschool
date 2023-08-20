c560083cfa68de8aedacf04e1b62febe3d1e46a0
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-20 09:59:22 +0300

    Update exercise6-fix.sh

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+echo "FILES: ${@}"
+FILES="$@"
+echo "---------------------------"
+echo "LAST: ${@: -1}"
+LAST="${@: -1}"
+echo "--------------------------"
+
+        for f in $FILES
+        do
+                if [ "$f" = "$LAST" ];then
+                        echo "we got to the last argument"
+                        break
+
+                fi
+                echo "sudo cp $f $LAST "
+                sudo cp $f $LAST
+        done
+
