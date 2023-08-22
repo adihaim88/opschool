a5bcde1ecacea279a6552f84615e5fb2b690484c
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-23 00:05:58 +0300

    Update exercise3-fix.sh

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo chmod /etc/apache2/sites-available/000-default.conf
-sudo sed '32,35d' /etc/apache2/sites-available/000-default.conf
+sudo chmod 777 /etc/apache2/sites-available/000-default.conf
+sudo sed -i -e '32,34d' /etc/apache2/sites-available/000-default.conf
 sudo sed -i -e "172s/None/All/g" /etc/apache2/apache2.conf
 
 sudo systemctl restart apache2
