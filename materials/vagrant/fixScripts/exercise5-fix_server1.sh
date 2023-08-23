9aa94b35d2fd803e44814b9d4c3d35fcd2122f9e
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-23 13:37:44 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#add fix to exercise5-server1 here
+
+sudo  chmod 777 /etc/hosts
+sudo echo "192.168.60.11 server2" >> /etc/hosts
 ssh-keygen -t rsa -b 4096
 eval "$(ssh-agent -s)"
 ssh-add  ./id_rsa
+
