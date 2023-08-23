7424a4a61dae2301ae02e69d276b486e10f75fba
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-23 14:23:48 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -4,4 +4,5 @@ chmod 600 ~/.ssh/authorized_keys
 
 sudo sed -i -e "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
 sudo sed -i -e "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config
+sudo service ssh restart
 
