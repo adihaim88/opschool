50a3c17cf2cd799354a7b983ebb60e051543f761
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-26 21:47:00 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -2,7 +2,8 @@
 
 sudo  chmod 777 /etc/hosts
 sudo echo "192.168.60.11 server2" >> /etc/hosts
- ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+chmod 600 ~/.ssh/authorized_keys
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 eval "$(ssh-agent -s)"
 ssh-add  ~/.ssh/id_rsa
 ssh-copy-id -i ~/.ssh/id_rsa.pub "vagrant@server2"
