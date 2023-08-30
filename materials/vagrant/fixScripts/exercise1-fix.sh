43c30303ab122800ed4adec59078f70be38b77ed
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-30 21:57:42 +0300

    Update exercise1-fix.sh

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "--------------------ex1------------------------
+
 
 sudo sed -i -e "s/#DNS=/DNS=192.168.1.1/g"  /etc/systemd/resolved.conf
 sudo sed -i -e "s/#FallbackDNS=/FallbackDNS=192.168.60.10 192.168.1.1/g"  /etc/systemd/resolved.conf
