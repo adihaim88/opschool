4fb11bc3d67931e00adb2cc394341401bac04b24
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-30 22:11:11 +0300

    Update exercise2-fix.sh

--- materials/vagrant/fixScripts/exercise2-fix.sh
+++ materials/vagrant/fixScripts/exercise2-fix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#add fix to exercise2 here
+echo "--------------------ex2------------------------"
 sudo  chmod 777 /etc/hosts
 sudo sed -i -e "s/127.0.0.1/#127.0.0.1/g" /etc/hosts
 sudo systemctl restart systemd-resolved
