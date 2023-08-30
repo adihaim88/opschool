a585a5222ae780815d05e19cc6b514bc040e0c09
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-30 22:11:55 +0300

    Update exercise4-fix_server1.sh

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#add fix to exercise4-server1 here
+
+echo "--------------------exercise4-server1------------------------"
+
 sudo  chmod 777 /etc/hosts
 sudo echo "192.168.60.11 server2" >> /etc/hosts
