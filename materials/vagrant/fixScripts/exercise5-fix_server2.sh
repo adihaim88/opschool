db4011fd9f6943a99a22b12224dd8305866675f2
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-31 00:17:18 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -8,9 +8,9 @@ chmod 600 ~/.ssh/authorized_keys
 sudo  chmod 777 /etc/hosts
 sudo echo "192.168.60.10 server1" >> /etc/hosts
 
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
-eval "$(ssh-agent -s)"
-ssh-add  ~/.ssh/id_rsa
+#ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+#eval "$(ssh-agent -s)"
+#ssh-add  ~/.ssh/id_rsa
 
 sudo sed -i -e "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
 sudo sed -i -e "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config
