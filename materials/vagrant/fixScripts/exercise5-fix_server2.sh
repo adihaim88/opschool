d0a201b8fe0a37afd64db3dca842fef0fb4fa600
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-31 09:49:43 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -5,3 +5,8 @@ echo "--------------------exercise5-server2------------------------"
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 
+sudo  chmod 777 /etc/hosts
+sudo echo "192.168.60.10 server1" >> /etc/hosts
+sshpass -p "vagrant" ssh "vagrant@server1" "echo '~/.ssh/id_rsa.pub' >> ~/.ssh/authorized_keys"
+
+
