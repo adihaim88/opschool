7e31edbfb71f199c1121e73f94ef1adb33736b82
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-31 13:52:58 +0300

    Update exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -12,8 +12,7 @@ sudo sed -i -e "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh
 sudo sed -i -e "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config
 sudo service ssh restart
 
-su - vagrant
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+sudo -u vagrant ssh-keygen -t rsa -b 4096 -f /home/vagrant/.ssh/id_rsa -N ""
 eval "$(ssh-agent -s)"
 ssh-add  ~/.ssh/id_rsa
 
