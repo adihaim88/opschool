b9581d811f10d4ac491be91d33ba17ca739e0a36
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-31 08:05:09 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -2,25 +2,4 @@
 echo "--------------------exercise5-server1------------------------"
 
 
-sudo  chmod 777 /etc/hosts
-sudo echo "192.168.60.11 server2" >> /etc/hosts
-chmod 600 ~/.ssh/authorized_keys
-
-sudo ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
-#su - vagrant -c 'ssh-keygen -t rsa -b 4096  -f ~/.ssh/id_rsa -N ""'
-
-eval "$(ssh-agent -s)"
-ssh-add  ~/.ssh/id_rsa
-
-#sudo sed -i -e "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
-sudo sed -i -e "s/#PasswordAuthentication/PasswordAuthentication/g" /etc/ssh/sshd_config
-sudo service ssh restart
-
-
-echo -e "Host server2\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null" >> ~/.ssh/config
-
-
-#sudo apt-get install sshpass
-#sshpass -p 'vagrant' ssh-copy-id vagrant@server2
-
 
