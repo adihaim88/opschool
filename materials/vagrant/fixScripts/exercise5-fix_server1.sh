825151e19c3d03e75a98e0b8035740096ffd08e8
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-31 09:42:38 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 echo "--------------------exercise5-server1------------------------"
+sudo  chmod 777 /etc/hosts
+sudo echo "192.168.60.11 server2" >> /etc/hosts
+chmod 600 ~/.ssh/authorized_keys
 
+sudo ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+eval "$(ssh-agent -s)"
+ssh-add  ~/.ssh/id_rsa
+
+sudo sed -i -e "s/PubkeyAuthentication no/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
+sudo service ssh restart
+
+
+echo -e "Host server2\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null" >> ~/.ssh/config
 
 
