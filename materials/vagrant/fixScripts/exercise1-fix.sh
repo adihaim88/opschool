d5e81530777a8e2efc04815541a3b309b3313857
adihaim88 <137032427+adihaim88@users.noreply.github.com>
2023-08-31 13:44:32 +0300

    Update exercise1-fix.sh

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -2,8 +2,5 @@
 
 echo "--------------------ex1------------------------"
 
-
-#sudo sed -i -e "s/#DNS=/DNS=192.168.1.1/g"  /etc/systemd/resolved.conf
-#sudo sed -i -e "s/#FallbackDNS=/FallbackDNS=192.168.60.10 192.168.1.1/g"  /etc/systemd/resolved.conf
 echo "nameserver 192.168.1.1" | sudo tee /etc/resolv.conf.head
 sudo systemctl restart systemd-networkd
